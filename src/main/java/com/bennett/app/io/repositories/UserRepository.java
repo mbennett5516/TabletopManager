3be4f28b092c7fc6f42a33f3611f2a8710fa4f23
Mark <mbennett5516@yahoo.com>
2021-04-26 13:43:30 -0500

    implemented get user details method

--- src/main/java/com/bennett/app/io/repositories/UserRepository.java
+++ src/main/java/com/bennett/app/io/repositories/UserRepository.java
@@ -8,4 +8,6 @@ import com.bennett.app.io.entities.UserEntity;
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 	UserEntity findByEmail(String email);
+
+	UserEntity findByUserId(String userId);
 }
