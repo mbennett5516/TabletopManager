c4efba8da330e843a7181cd2dbc3a224feb9c57f
Mark <mbennett5516@yahoo.com>
2021-04-27 18:31:08 -0500

    changed to war

--- src/main/java/com/bennett/app/TabletopManagerApplication.java
+++ src/main/java/com/bennett/app/TabletopManagerApplication.java
@@ -2,11 +2,13 @@ package com.bennett.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class TabletopManagerApplication {
+public class TabletopManagerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TabletopManagerApplication.class, args);
@@ -20,5 +22,10 @@ public class TabletopManagerApplication {
 	@Bean
 	SpringApplicationContext springApplicationContext() {
 		return new SpringApplicationContext();
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TabletopManagerApplication.class);
 	}
 }
